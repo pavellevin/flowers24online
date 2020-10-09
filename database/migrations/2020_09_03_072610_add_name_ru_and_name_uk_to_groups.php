2151da923be1e83f9ba07444bd8712ff62548168
pavellevin <levin@exp1.net>
2020-10-09 11:24:42 +0200

    add migration for lang

---
+++ database/migrations/2020_09_03_072610_add_name_ru_and_name_uk_to_groups.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNameRuAndNameUkToGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->string('name_ru', 100)->after('name_en');
+            $table->string('name_uk', 100)->after('name_ru');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->dropColumn('name_ru');
+            $table->dropColumn('name_uk');
+        });
+    }
+}
